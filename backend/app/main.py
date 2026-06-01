8d5dfeb76d44639f1420d8c7adfb25ca7691effe
bangaruyogendra <bangaruyogendra66@gmail.com>
2026-06-01 09:00:29 +0530

    frontend and backend connection setup complete

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.app.api.query import router as query_router
 from backend.app.api.ingestion import router as ingestion_router
 from backend.app.api.generate_embedding import router as embedding_router
@@ -7,6 +8,18 @@ app = FastAPI(
     title = "Enterprise RAG Application"   
 )
 
+origins = [
+    "http://localhost:5173",  
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(query_router)
 app.include_router(ingestion_router)

b1dfbfcf9f9bf83261a0c45d96b6388417e92daa
bangaruyogendra <bangaruyogendra66@gmail.com>
2026-06-01 07:44:00 +0530

    embedding router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from backend.app.api.query import router as query_router
 from backend.app.api.ingestion import router as ingestion_router
+from backend.app.api.generate_embedding import router as embedding_router
 
 app = FastAPI(
     title = "Enterprise RAG Application"   
@@ -9,6 +10,7 @@ app = FastAPI(
 
 app.include_router(query_router)
 app.include_router(ingestion_router)
+app.include_router(embedding_router)
 
 
 @app.get("/health")
